6480e7edfbf5450acd4b1c112f104ca90b000082
-g <-g>
2021-06-24 21:41:02 -0300

    Trocando de repo.

---
+++ src/main/java/com/gevichiatto/sisgu/SisguApplication.java
@@ -0,0 +1,13 @@
+package com.gevichiatto.sisgu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gevichiatto/sisgu/SisguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gevichiatto.sisgu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
